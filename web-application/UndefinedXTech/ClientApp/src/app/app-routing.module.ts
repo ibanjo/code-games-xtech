d7b0e5a7d1f6fc856d3b66dd45568204c8a2a467
Giuseppe CANNONE <giuseppe.cannone@alten.it>
2022-06-08 18:53:10 +0200

    fix

--- web-application/UndefinedXTech/ClientApp/src/app/app-routing.module.ts
+++ web-application/UndefinedXTech/ClientApp/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { LoginComponent } from './features/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
