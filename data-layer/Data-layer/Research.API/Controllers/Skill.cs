cae88a011830df9579a880a5791ea6ef9b4da994
m.mema <michela.mema@alten.it>
2022-06-08 12:03:16 +0200

    Added Api

---
+++ data-layer/Data-layer/Research.API/Controllers/Skill.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Research.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Skill : ControllerBase
+    {
+        // GET: api/<Skill>
+        [HttpGet]
+        public IEnumerable<Domain.Entity.Skill> Get()
+        {
+            return new ServiceLayer.GenericService.GenericService<Domain.Entity.Skill, Guid>().Get();
+        }
+
+        // GET api/<Skill>/5
+        [HttpGet("{id}")]
+        public Domain.Entity.Skill Get(Guid id)
+        {
+            return new ServiceLayer.GenericService.GenericService<Domain.Entity.Skill, Guid>().GetById(id);
+        }
+
+        // POST api/<Skill>
+        [HttpPost]
+        public void Post([FromBody] Domain.Entity.Skill value)
+        {
+            new ServiceLayer.GenericService.GenericService<Domain.Entity.Skill, Guid>().Insert(value);
+        }
+    }
+}
